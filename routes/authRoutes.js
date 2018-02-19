7a9f327664536518a513b1aee56453c474ba4d36
David Ladowitz <david@ladowitz.com>
2018-02-19 09:21:03 -0800

    finish oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,20 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.send("Logged In Propperly");
+    }
+  );
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 };
